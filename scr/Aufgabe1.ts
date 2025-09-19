25e1ae60ad86078d4f923f51f756fa061b3f3805
Aleynaolgun <Aleyna.Olgun@swb-gruppe.de>
2025-09-19 14:37:43 +0200

    Update Aufgabe1.ts

--- scr/Aufgabe1.ts
+++ scr/Aufgabe1.ts
@@ -1,12 +1,24 @@
 
 // Aufgabe 1 Rabatt
-let price=90 //man muss den Geldbetrag selbst eingeben.
-let isMember: boolean = false;  //man muss true/ false selber eingeben (ob es ein Member sein soll oder nicht)
- if(isMember && price>100) {
+let price=150 //man muss den Geldbetrag selbst eingeben.
+let isMember: boolean = true;
+let isNotMember: boolean = false;
+if(isMember && price>100) {
      console.log("Es gibt 20% Rabatt", price*0.8);
- } else( isMember&& price>100 || isMember && price<100)
-     console.log("Es gibt 10% Rabatt, ab 100", price*0.9,
-          "Es gibt kein Rabatt, unter 100", price);
+ }
+ if( isNotMember && price>100) {
+    console.log("Es gibt 10% Rabatt", price * 0.9);
+
+}
+if(isNotMember && price<100){
+     console.log("Es gibt keinen Rabatt", price);}
+
+
+
+
+ //else( isMember&& price>100 || isMember && price<100)
+    // console.log("Es gibt 10% Rabatt, ab 100", price*0.9,
+          //"Es gibt kein Rabatt, unter 100", price);
  {
 
 

87e479be1aedb65fe61eeeb952a286fe3fbb8364
Aleynaolgun <Aleyna.Olgun@swb-gruppe.de>
2025-09-19 13:47:57 +0200

    Create Aufgabe 2.ts

---
+++ scr/Aufgabe 2.ts
@@ -0,0 +1,20 @@
+//Rechenoperator
+let a=10;
+let b=4;
+let operation= "divide"
+if(operation=="add"){
+    console.log(a+b);
+}
+if(operation=="subtract"){
+    console.log(a-b);
+}
+
+if(operation=="multiply"){
+    console.log(a*b);
+}
+
+if(operation=="divide"){
+    console.log(a/b);
+}
+
+

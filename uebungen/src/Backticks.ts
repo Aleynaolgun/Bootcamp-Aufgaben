9b218696d868509fff4bb79b08ace0385d709958
Aleynaolgun <Aleyna.Olgun@swb-gruppe.de>
2025-12-04 11:34:27 +0100

    Backticks
    Kurzer Exkurs, um die Schreibweise zu verstehen.

---
+++ uebungen/src/Backticks.ts
@@ -0,0 +1,8 @@
+// kurzer exkurs um Strings und variablen besser zusammenzubauen
+
+const marke = "Audi";
+const modell = "R8";
+const jahr = 2024;
+
+const neuerSatz = `Ich fahre einen ${marke} ${modell} aus dem Jahr ${jahr}.`;
+console.log(neuerSatz);
